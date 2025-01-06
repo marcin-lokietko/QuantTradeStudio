4c6f6891d1478b83925429fabbc5ed71f9c615f2
Marcin Łokietko <marcin.lokietko@gmail.com>
2025-01-06 20:49:20 +0100

    Add first call to Binance testnet api: GET /api/v3/time

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ void setupLogger(const char* programName, const char* logDir) {
   LOG(INFO) << "Glog logger successfully initialized";
 }
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp) {
   const size_t contentSizeBytes = size * nmemb;
   static_cast<std::string*>(userp)->append(static_cast<char*>(contents), contentSizeBytes);
   return contentSizeBytes;
@@ -31,7 +31,7 @@ std::string getBinancePrice(const std::string& symbol) {
     curl_global_cleanup();
   }
   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, &receivedData);
 
   const auto res = curl_easy_perform(curl);
@@ -44,12 +44,7 @@ std::string getBinancePrice(const std::string& symbol) {
   return receivedData;
 }
 
-int main(int, char* argv[]) {
-  setupLogger(argv[0], argv[1]);
-
-  LOG(INFO) << "########## Starting AlgoTrader";
-
-  const std::string symbol = "BTCUSDT";
+void printBinancePrice(const std::string& symbol) {
   const std::string response = getBinancePrice(symbol);
 
   const auto jsonResponse = nlohmann::json::parse(response);
@@ -59,6 +54,37 @@ int main(int, char* argv[]) {
   } else {
     LOG(ERROR) << "Failed to retrieve price.";
   }
+}
+
+std::string runHttpGet(const std::string& url) {
+  CURL* curl = curl_easy_init();
+  std::string response;
+
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, NULL);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+  }
+  return response;
+}
+
+int main(int, char* argv[]) {
+  setupLogger(argv[0], argv[1]);
+
+  LOG(INFO) << "########## Starting AlgoTrader";
+
+  const std::string symbol = "BTCUSDT";
+  printBinancePrice(symbol);
+
+  // Test connectivity with Binance testnet
+  const std::string binanceTestnetBaseUrl = "https://testnet.binance.vision";
+  const std::string timeUrl = binanceTestnetBaseUrl + "/api/v3/time";
+  const std::string timeResponse = runHttpGet(timeUrl);
+
+  LOG(INFO) << "Binance testnet GET /api/v3/time response: " << timeResponse;
 
   LOG(INFO) << "########## Ending AlgoTrader";
 }
